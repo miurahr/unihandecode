09e5d43e42df19f3fe33036b22bcce7e62f8d24d
Hiroshi Miura <miurahr@linux.com>
2014-09-26 09:18:45 +0900

    mkdist.sh: remove python path for virtualenv
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- mkdist.sh
+++ mkdist.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 make clean
-make PYTHON=/usr/bin/python2 test
-make PYTHON=/usr/bin/python2 bdist
+make PYTHON=python2 test
+make PYTHON=python2 bdist
 make sdist
 make PYTHON=python3 test 
 make PYTHON=python3 bdist
